f6d08b8d42b20a8482a63e0cefc70bc410982b7b
Oleg Blishch <32680137+olegblishch88@users.noreply.github.com>
2019-07-08 20:32:05 +0300

    finish

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,66 @@
+let playGame = confirm('Do you want to play a game?');
+if (!playGame) {
+    alert('You did not become a billionaire, but can!');
+} else {
 
+    let attempts = 3;
+    let maxPrize = 100;
+    let range = 8;
+    let totalPrize = 0;
+    let four = 4;
+    let two = 2;
+    let three = 3;
+    do {
+        let possiblePrize = maxPrize;
+        let randomInteger = Math.floor(Math.random() * range + 1);
+        if (attempts === 1) {
+            possiblePrize = Math.floor(possiblePrize / four);
+        } else if (attempts === two) {
+            possiblePrize = Math.floor(possiblePrize / two);
+        }
+        let askUser = Number(prompt(`
+        Choose a roulette pocket number from 0 to ${range} \n
+        Attepts left: ${attempts} \n
+        Total prize: ${totalPrize} \n
+        Possible prize on current attempt: ${possiblePrize}`));
+        if (askUser === randomInteger && attempts === three) {
+            alert('Congratulation, you won! Your prize is:' + possiblePrize);
+            if (confirm('Do you want to continue?')) {
+                totalPrize += maxPrize;
+                maxPrize *= two;
+                attempts = three;
+                range += four;
+                continue;
+            } else {
+                alert('Thank you for your participation. Your prize is: ' + possiblePrize);
+                break;
+            }
+        } else if (askUser === randomInteger && attempts === two) {
+            alert('Congratulation, you won! Your prize is:' + possiblePrize);
+            if (confirm('Do you want to continue?')) {
+                totalPrize += maxPrize / two;
+                maxPrize *= two;
+                attempts = three;
+                range += four;
+                continue;
+            } else {
+                alert('Thank you for your participation. Your prize is: ' + possiblePrize);
+                break;
+            }
+        } else if (askUser === randomInteger && attempts === 1) {
+            alert('Congratulation, you won! Your prize is:' + possiblePrize);
+            if (confirm('Do you want to continue?')) {
+                totalPrize += maxPrize / four;
+                maxPrize *= two;
+                attempts = three;
+                range += four;
+                continue;
+            } else {
+                alert('Thank you for your participation. Your prize is: ' + possiblePrize);
+                break;
+            }
+        }
+        attempts--;
+    }
+    while (attempts > 0);
+}
