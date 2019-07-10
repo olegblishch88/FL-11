05109756b0d8425551e42fa228f5484efe7cef9b
Oleg Blishch <32680137+olegblishch88@users.noreply.github.com>
2019-07-11 02:29:00 +0300

    add

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,3 +1,7 @@
-function formatTime(){
-
+function formatTime(min){
+    let days = Math.floor(min / 1440);
+    let hours = Math.floor((min - days * 1440) / 60);
+    let minutes = min -days * 1440 - hours * 60;
+    return days + "day(s) " + hours + " hour(s) " + minutes + " minute(s)";
 }
+formatTime(3000);
